b14593a15d7d09de37f3be9204ac106f71dee65c
Bkeenke <tynay@bakasov.kg>
2025-12-26 13:58:13 +0300

    обновление

--- entry.sh
+++ entry.sh
@@ -4,7 +4,15 @@
 [ -z "$SHM_HOST" ] || sed -i "s|http://shm.local|$SHM_HOST|g" /etc/nginx/http.d/default.conf
 [ -z "$RESOLVER" ] || sed -i "s|resolver 127.0.0.11|resolver $RESOLVER|g" /etc/nginx/http.d/default.conf
 
+if [ ! -z "$SHM_BASE_PATH" ] && [ "$SHM_BASE_PATH" != "/" ]; then
+    sed -i "s|location / {|location $SHM_BASE_PATH {|" /etc/nginx/http.d/default.conf
+    sed -i "s|location /shm {|location $SHM_BASE_PATH/shm {|" /etc/nginx/http.d/default.conf
+    sed -i "s|#proxy_cookie_path;|proxy_cookie_path / $SHM_BASE_PATH;|" /etc/nginx/http.d/default.conf
+    sed -i "s|<base href=\"/\"|<base href=\"$SHM_BASE_PATH/\"|" /app/index.html
+fi
+
 echo "Starting nginx with configuration:"
 echo "  SHM_HOST: ${SHM_HOST:-http://shm.local}"
+echo "  SHM_BASE_PATH: ${SHM_BASE_PATH:-/}"
 
 exec nginx -g 'daemon off;'
