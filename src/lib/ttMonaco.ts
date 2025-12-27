14c37bf4abba756843d9760c5f08faa1630d3e91
zakachkin <viktordemo@gmail.com>
2025-12-27 15:08:55 +0300

    Показать snippet descriptions in completion меню

--- src/lib/ttMonaco.ts
+++ src/lib/ttMonaco.ts
@@ -205,7 +205,10 @@ export function registerTTCompletion(monaco: any) {
     const body = normalizeBody(snippet.body);
     const prefixes = normalizePrefixes(snippet.prefix);
     return prefixes.map((prefix) => ({
-      label: prefix,
+      label: {
+        label: String(prefix),
+        description: snippet.description || key,
+      },
       kind: monaco.languages.CompletionItemKind.Snippet,
       detail: snippet.description || key,
       documentation: snippet.description || key,
